e89b25e6c70fd90b81cc673047b51593e397eba2
Sahil17102 <Sahil17102@users.noreply.github.com>
2026-09-18 17:26:35 +0530

    Make navigation compatible with static hosting

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useParams } from 'next/navigation';
-import Link from 'next/link';
+import Link from '@/components/static-link';
 import { useState } from 'react';
 import {
   ArrowRight,
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';
+import Link from '@/components/static-link';
 import { useEffect, useState } from 'react';
 import {
   ArrowRight,
